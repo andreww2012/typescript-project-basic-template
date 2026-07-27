443d057961c6c05c43759757451745cc0be0845e
Andrew Kazakov <andreww_dev@proton.me>
2026-07-27 16:12:26 +0300

    feat(eslint): enable `markdown/sentencesPerLine` config by default

--- eslint.config.ts
+++ eslint.config.ts
@@ -6,6 +6,9 @@ export default eslintConfig({
   //   allowDefaultProject: ['*.config.*ts'],
   // },
   configs: {
+    markdown: {
+      configSentencesPerLine: true,
+    },
     // noStylisticRules: true,
     // noStylisticRules: {
     //   enableRules: {
