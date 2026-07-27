b968c4b2ed50c11f41f1fc79f7d0f04a4b445a92
Andrew Kazakov <andreww_dev@proton.me>
2026-07-27 16:11:12 +0300

    feat(eslint): extend config with more predefined potentially useful config options

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,13 +2,28 @@ import {eslintConfig} from 'eslint-config-un';
 
 export default eslintConfig({
   // defaultConfigsStatus: 'misc-enabled',
+  // typeInfoRules: {
+  //   allowDefaultProject: ['*.config.*ts'],
+  // },
   configs: {
-    ts: {
-      // allowDefaultProject: ['*.config.*ts'],
-    },
+    // noStylisticRules: true,
+    // noStylisticRules: {
+    //   enableRules: {
+    //     rules: true,
+    //     disableAllOtherRules: true,
+    //   },
+    // },
+    // ts: {
+    //   configDisableNoUnsafe: true,
+    //   configNoTypeAssertion: true,
+    //   overrides: {
+    //     'ts/no-explicit-any': 0,
+    //   },
+    // },
 
     // False positives:
     // youDontNeedLodashUnderscore: false, // cspell:disable-line
     zod: false,
   },
+  extraConfigs: [],
 });
